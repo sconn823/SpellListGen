4d59a5e9198c0584e34ccaba861280c3df3a446f
Sean <sconn823@gmail.com>
2019-07-19 10:53:37 -0400

    Changed sorting script to script by class

--- sortSpells.py
+++ sortSpells.py
@@ -1,45 +1,26 @@
 import json
 import requests
 
-with open("monstersComplete.json", 'r') as inputFile:
+with open("spellsComplete.json", 'r') as inputFile:
     fullList = json.load(inputFile)
 
 categorizedList = {
-    0.125: [],
-    0.25: [],
-    0.5: [],
-    0: [],
-    1: [],
-    2: [],
-    3: [],
-    4: [],
-    5: [],
-    6: [],
-    7: [],
-    8: [],
-    9: [],
-    10: [],
-    11: [],
-    12: [],
-    13: [],
-    14: [],
-    15: [],
-    16: [],
-    17: [],
-    18: [],
-    19: [],
-    20: [],
-    21: [],
-    22: [],
-    23: [],
-    24: [],
-    30: []
+    "Wizard": [],
+    "Bard": [],
+    "Cleric": [],
+    "Paladin": [],
+    "Sorcerer": [],
+    "Warlock": [],
+    "Ranger": [],
+    "Druid": []
 }
 for obj in fullList:
-    try:
-        categorizedList[obj["challenge_rating"]].append(obj)
-    except KeyError as e:
-        print(e)
+    classes = obj['classes']
+    for dude in classes:
+        try:
+            categorizedList[dude["name"]].append(obj)
+        except KeyError as e:
+            print(e)
 
-with open("crSortedMonsters.json", "w") as output:
+with open("classSortedSpells.json", "w") as output:
     json.dump(categorizedList, output)
